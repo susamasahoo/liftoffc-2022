365474155515f15e4034658e9d6e66532af905af
SONALI SAHOO <98395654+susamasahoo@users.noreply.github.com>
2022-02-11 22:41:34 +0530

    Add files via upload

---
+++ week-1_q1.c.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    float Name, Regd , Branch , Hobbiess;
+    printf("Name:Sonali Sahoo\n");
+    printf("Regd:070\n");
+    printf("Branch:Eletrical Engineering\n");
+    printf("Hobbies: Drawing,Dancing\n");
+    return 0;
+}
+
+
